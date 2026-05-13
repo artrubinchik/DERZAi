a601d0301ce1b018bb2228d2b987692d1fc7a7e0
artrubinchik <artrubinchik@gmail.com>
2026-05-13 14:16:12 +0300

    Refactor Footer component comments and SVG attributes
    Updated comments and adjusted SVG dimensions in Footer component.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,19 +6,19 @@ function FooterLogo() {
       {/* Верхняя форма */}
       <div className="absolute left-0 top-[8px] h-[56px] w-[92px] rounded-r-full border border-current bg-current" />
 
-      {/* Нижний квадрат */}
+      {/* Квадрат */}
       <div className="absolute bottom-0 left-0 h-[50px] w-[50px] border border-current bg-current" />
 
       {/* Треугольник */}
       <svg
-        className="absolute bottom-0 left-[54px]"
+        className="absolute bottom-[1px] left-[54px]"
         width="50"
-        height="50"
-        viewBox="0 0 50 50"
+        height="49"
+        viewBox="0 0 50 49"
         fill="currentColor"
       >
         <path
-          d="M1 49V1L49 49H1Z"
+          d="M1 48V1L49 48H1Z"
           stroke="currentColor"
           strokeWidth="1"
         />
